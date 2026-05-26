4d1a2008a2c5fb28ad4941ae3a093b3490cee20c
Nicolas COCHIN <github@account.rocks>
2026-05-26 11:57:46 +1000

    Create fonts.sh

---
+++ install/fonts.sh
@@ -0,0 +1,16 @@
+FONT_DIR=${HOME}/.local/share/fonts
+mkdir -p $FONT_DIR
+declare -A FONTS=(
+  ["MesloLGS NF Regular.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+  ["MesloLGS NF Bold.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+  ["MesloLGS NF Italic.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+  ["MesloLGS NF Bold Italic.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+)
+for FONT_NAME in "${!FONTS[@]}"; do 
+  FONT_URL="${FONTS[$FONT_NAME]}"
+  DEST="${FONT_DIR}/${FONT_NAME}"
+  ! test -f "$DEST" && (
+    echo "Install font $FONT_NAME..." && \
+    curl -fsSL ${FONT_URL} -o "${DEST}" \
+  ) || echo "Font $DEST already installed"
+done
