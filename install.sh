7e00f6fc193e6b619e0a2d6c03b438ab0013acbf
Nicolas COCHIN <github@account.rocks>
2026-05-26 11:39:49 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Give people a chance to retry running the installation
+trap 'echo "BluePill installation failed! You can retry by running: source ${BLUEPILL_LOCAL}/install.sh"' ERR
+
+# Install everything
+for f in ${BLUEPILL_LOCAL}/install/*.sh; do
+  echo -e "\nRunning installer: $f"
+  source "$f"
+done
