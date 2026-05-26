ebceaf9639bea0888fbe2fa6f4bb90ad8e266b15
Nicolas COCHIN <github@account.rocks>
2026-05-26 11:24:56 +1000

    Add boot script for Bluepill installation
    This script checks for the Fedora Silverblue environment, clones the Bluepill repository, and runs the installation script.

---
+++ boot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source /etc/os-release
+
+if [[ "${ID:-}" != "fedora" || "${VARIANT_ID:-}" != "silverblue" ]]; then
+    echo "This script must be run on Fedora Silverblue." >&2
+    exit 1
+fi
+
+clear
+BLUEPILL_REPO="${BLUEPILL_REPO:-nicolascochin/bluepill}"
+BLUEPILL_LOCAL="${BLUEPILL_LOCAL:~/.local/share/bluepill}"
+
+echo -e "\nCloning Bluepill from: https://github.com/${BLUEPILL_REPO}.git"
+rm -rf ${BLUEPILL_LOCAL}/
+git clone -q "https://github.com/${BLUEPILL_REPO}.git" ${BLUEPILL_LOCAL}
+
+echo -e "\nInstallation starting..."
+source ${BLUEPILL_LOCAL}/install.sh
