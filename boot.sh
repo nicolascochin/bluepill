ed0d63ec6e533a109d64566594a6083f1320d24a
Nicolas COCHIN <github@account.rocks>
2026-05-28 23:18:38 +1000

    Update boot.sh to use BLUEPILL_LOCAL variable

--- boot.sh
+++ boot.sh
@@ -11,10 +11,11 @@ fi
 
 clear
 BLUEPILL_REPO="${BLUEPILL_REPO:-nicolascochin/bluepill}"
+BLUEPILL_LOCAL="${BLUEPILL_LOCAL:-${HOME}/.local/share/bluepill}"
 
 echo -e "\nCloning Bluepill from: https://github.com/${BLUEPILL_REPO}.git"
-rm -rf ${HOME}/.local/share/bluepill/
-git clone -q "https://github.com/${BLUEPILL_REPO}.git" ${HOME}/.local/share/bluepill
+rm -rf ${BLUEPILL_LOCAL}/
+git clone -q "https://github.com/${BLUEPILL_REPO}.git" ${BLUEPILL_LOCAL}
 
 echo -e "\nInstallation starting..."
-source ${HOME}/.local/share/bluepill/install.sh
+source ${BLUEPILL_LOCAL}/install.sh
