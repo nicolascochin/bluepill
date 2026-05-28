f291ed2d2ad145ca34d6c1b3c12fed999f8281f7
Nicolas COCHIN <github@account.rocks>
2026-05-28 23:11:52 +1000

    Add script to update system using rpm-ostree

---
+++ install/01_update_system.sh
@@ -0,0 +1,7 @@
+if rpm-ostree upgrade --check 2> /dev/null | grep -q "AvailableUpdate"
+then     
+  echo "Updating the system"
+  rpm-ostree upgrade --reboot
+else     
+  echo "System is up 2 date"
+fi
