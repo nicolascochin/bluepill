a8c0363f8f9a215e64b44bb24a6438371b8d4fb3
Nicolas COCHIN <github@account.rocks>
2026-05-29 17:54:37 +1000

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -2,17 +2,10 @@
 
 set -euo pipefail
 
-source /etc/os-release
-
-if [[ "${ID:-}" != "fedora" || "${VARIANT_ID:-}" != "silverblue" ]]; then
-    echo "This script must be run on Fedora Silverblue." >&2
-    exit 1
-fi
-
-clear
 BLUEPILL_REPO="${BLUEPILL_REPO:-nicolascochin/bluepill}"
 BLUEPILL_LOCAL="${BLUEPILL_LOCAL:-${HOME}/.local/share/bluepill}"
 
+clear
 echo -e "\nCloning Bluepill from: https://github.com/${BLUEPILL_REPO}.git"
 rm -rf ${BLUEPILL_LOCAL}/
 git clone -q "https://github.com/${BLUEPILL_REPO}.git" ${BLUEPILL_LOCAL}
