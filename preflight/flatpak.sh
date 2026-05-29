702652ee8f8f33c298e1004f565344cb789a744a
Nicolas COCHIN <github@account.rocks>
2026-05-29 21:41:47 +1000

    Update flatpak.sh

--- preflight/flatpak.sh
+++ preflight/flatpak.sh
@@ -4,6 +4,8 @@ check_flathub() {
   flatpak remotes | grep -q flathub
 }
 
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 print_msg "Checking if flathub is enabled"
 if check_flathub; then
   print_status ok
