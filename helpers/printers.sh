f141df2621586bbe5d6f68a1a12ea12d2a81b96b
Nicolas COCHIN <github@account.rocks>
2026-05-29 17:39:24 +1000

    Add printer utility functions for messaging

---
+++ helpers/printers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+print_msg() {
+  printf "%s" "${1:?Usage: print_msg <message>}"
+}
+
+print_status() {
+  case "${1:?Usage: print_status <ok|ko>}" in
+    ok|OK) printf "[\e[32mOK\e[0m]\n" ;;
+    ko|KO) printf "[\e[31mKO\e[0m]\n" ;;
+    *)     printf "[??]\n" ;;
+  esac
+}
