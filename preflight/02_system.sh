e6892f14488445a0a85bc3c5b6fdb6234f240d7b
Nicolas COCHIN <github@account.rocks>
2026-05-29 21:38:18 +1000

    Update 02_system.sh

--- preflight/02_system.sh
+++ preflight/02_system.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
 print_msg "Checking if a system update is up to date"
 
-if rpm-ostree upgrade --check 2> /dev/null | grep -q "AvailableUpdate"
-then     
-  print_status ko
-  echo "Updating the system"
-  exit 1
-else     
-  print_status ok
+rpm_output=$(rpm-ostree upgrade --check 2>/dev/null)
+rpm_exit=$?
+
+if [ $rpm_exit -ne 0 ]; then
+    print_status ko
+    echo "System is currently upgrading. Retry later"
+    exit 1
+elif echo "$rpm_output" | grep -q "AvailableUpdate"; then
+    print_status ko
+    echo "Updating the system"
+    exit 1
+else
+    print_status ok
 fi
