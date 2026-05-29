21996d5c5118d8efefd4de6c07cf1776132d9a7a
Nicolas COCHIN <github@account.rocks>
2026-05-29 21:48:28 +1000

    Update 01_fedora_silverblue_only.sh

--- preflight/01_fedora_silverblue_only.sh
+++ preflight/01_fedora_silverblue_only.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 source /etc/os-release
+print_msg "Checking system is a silverblue"
 
 if [[ "${ID:-}" != "fedora" || "${VARIANT_ID:-}" != "silverblue" ]]; then
-    echo "This script must be run on Fedora Silverblue." >&2
-    exit 1
+  print_status ko
+  echo "This script must be run on Fedora Silverblue." >&2
+  exit 1
+else 
+  print_status ok
 fi
 
