e9e95f369f797f278beba99a08d98dd634e50563
Nicolas COCHIN <github@account.rocks>
2026-06-25 22:48:03 +1000

    Add script to install Papirus icon theme

---
+++ packages/icons.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FONT_DIR=${HOME}/.local/share/icons
+TMP_DIR=$(mktemp -d)
+(
+  print_msg "📥 Installing papirus icons"
+  cd "$TMP_DIR"
+  curl -L -o papirus.tar.gz https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/refs/heads/master.tar.gz
+  tar -xzf papirus.tar.gz
+  mkdir -p $FONT_DIR
+  cp -r papirus-icon-theme-master/Papirus* $FONT_DIR \
+    && print_status ok \
+    || print_status ko
+)
