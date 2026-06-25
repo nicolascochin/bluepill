6b3e71ba3a93d662354c6ce1b9b539466ec34149
Nicolas COCHIN <github@account.rocks>
2026-06-25 23:34:19 +1000

    Update copier.sh

--- helpers/copier.sh
+++ helpers/copier.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 
 copy_files() {
-  local src_folder="${1:?Usage: copy_files <folder>}"
-  local dest_folder="${2:?Usage: copy_files <folder>}"
+  local src_folder="${1:?Usage: copy_files <src_folder> <dest_folder>}"
+  local dest_folder="${2:?Usage: copy_files <src_folder> <dest_folder>}"
+
+  mkdir -p $dest_folder
 
   for f in "${BLUEPILL_LOCAL}/${src_folder}"/*; do
     [[ -f "$f" ]] || continue
-    print_msg "📥 Copying $f into ${dest_folder}/"
-    cp $f ${dest_folder} && print_status ok || print_status ko
+    print_msg "📥 Copying $f into ${dest_folder}"
+    cp $f ${dest_folder}/ && print_status ok || print_status ko
   done
 }
