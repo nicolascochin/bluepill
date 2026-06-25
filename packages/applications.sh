b70392e28591ddf7000af5b1cc7638ce282afba8
Nicolas COCHIN <github@account.rocks>
2026-06-25 12:12:51 +1000

    Update applications.sh

--- packages/applications.sh
+++ packages/applications.sh
@@ -2,12 +2,17 @@
 
 declare -A APPS=(
   ["com.protonvpn.www"]="Proton VPN"
+  ["me.proton.Pass"]="Proton Pass"
+  ["me.proton.Mail"]="Proton Mail"
   ["com.spotify.Client"]="Spotify"
   ["org.videolan.VLC"]="VLC"
   ["com.brave.Browser"]="Brave Browser"
   ["io.podman_desktop.PodmanDesktop"]="Podman Desktop"
   ["rest.insomnia.Insomnia"]="Insomnia"
   ["com.slack.Slack"]="Slack"
+  ["com.discordapp.Discord"]="Discord"
+  ["io.github.CyberTimon.RapidRAW"]="Rapid RAW"
+  ["com.visualstudio.code"]="Visual Studio Code"
 )
 for KEY in "${!APPS[@]}"; do 
   NAME="${APPS[$KEY]}"
@@ -20,5 +25,11 @@ for KEY in "${!APPS[@]}"; do
       && print_status ok \
       || print_status ko
   fi
+done
+
+for KEY in flatpak list --app --columns=application,origin | awk '$2=="fedora"{print $1}'
+do
+  print_msg "Re-installing $KEY from flathub"
+  print_status ok
 done
 
