7d0da287a1a2b9f978e1387911b49303e7b14abc
Nicolas COCHIN <github@account.rocks>
2026-06-25 13:49:30 +1000

    Update fonts.sh

--- packages/fonts.sh
+++ packages/fonts.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
 declare -A FONTS=(
-  ["MesloLGS NF Regular.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
-  ["MesloLGS NF Bold.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
-  ["MesloLGS NF Italic.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
-  ["MesloLGS NF Bold Italic.ttf"]="https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+  ["FiraCode"]="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip"
+  ["Meslo"]="https://github.com/romkatv/powerlevel10k-media/releases/download/v2.3.3/meslo-lgs-nf.tar.gz"
 )
-FONT_DIR=${HOME}/.local/share/fonts
 
-mkdir -p $FONT_DIR
+download_and_install_font() {
+  local NAME="$1"
+  local URL="$2"
 
-for FONT_NAME in "${!FONTS[@]}"; do 
-  FONT_URL="${FONTS[$FONT_NAME]}"
-  print_msg "Installing font $FONT_NAME"
-  curl -fsSL "$FONT_URL" -o "${FONT_DIR}/${FONT_NAME}" \
-    && print_status ok \
-    || print_status ko
+  local TMP_ZIP="/tmp/${NAME}.zip"
+
+  print_msg "📥 Installing $NAME"
+  if curl -fsSL "$URL" -o "$TMP_ZIP"; then
+    unzip -oq "$TMP_ZIP" -d "$FONT_DIR"
+    rm -f "$TMP_ZIP"
+    print_status ok
+  else
+    rm -f "$TMP_ZIP"
+    print_status ko
+    return 1
+  fi
+}
+
+for FONT_NAME in "${!FONTS[@]}"; do
+  download_and_install_font "$FONT_NAME" "${FONTS[$FONT_NAME]}"
 done
+
+print_msg "🔄 Refreshing font cache"
+fc-cache -fv >/dev/null && print_status ok || print_status ko
+
+
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
+unzip NerdFontsSymbolsOnly.zip -d ~/.local/share/fonts
+fc-cache -fv
