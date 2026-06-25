287669efcce3fc3a47d708a4e36a3096a77f97c6
Nicolas COCHIN <github@account.rocks>
2026-06-25 23:20:47 +1000

    Add script to create web application desktop entry
    This script creates a desktop entry for a web application with specified name, URL, and icon.

---
+++ bin/create_webapp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+APP_NAME="$1"
+APP_URL="$2"
+APP_ICON="$3"
+
+APP_DIR=${HOME}/.local/share/applications
+DESKTOP_ID="$(echo "$APP_NAME" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')"
+
+mkdir -p $APP_DIR
+
+cat > ${APP_DIR}/${DESKTOP_ID}.desktop <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=${APP_NAME}
+Exec=xdg-open ${APP_URL}
+Icon=web-browser
+Terminal=false
+Categories=Network;
+EOF
+
+chmod +x ~/.local/share/applications/${DESKTOP_ID}.desktop
+
