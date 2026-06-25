dc8685cc3bc59f84fd059a47ff00fbb4029d75d1
Nicolas COCHIN <github@account.rocks>
2026-06-25 23:26:31 +1000

    Create copier.sh

---
+++ helpers/copier.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+copy_files() {
+  local src_folder="${1:?Usage: copy_files <folder>}"
+  local dest_folder="${1:?Usage: copy_files <folder>}"
+
+  for f in "${BLUEPILL_LOCAL}/${src_folder}"/*; do
+    [[ -f "$f" ]] || continue
+    print_msg "📥 Copying $f into ${dest_folder}"
+    cp $f ${dest_folder} && print_status ok || print_status ko
+  done
+}
