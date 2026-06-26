b7b874a881ae8a995d9c1bc08a5c645d31c206ee
Nicolas COCHIN <github@account.rocks>
2026-06-26 18:18:03 +1000

    Update icons.sh

--- packages/icons.sh
+++ packages/icons.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 FONT_DIR=${HOME}/.local/share/icons
-TMP_DIR=$(mktemp -d)
-(
-  print_msg "📥 Installing papirus icons"
-  cd "$TMP_DIR"
-  curl -sL -o papirus.tar.gz https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/refs/heads/master.tar.gz
-  tar -xzf papirus.tar.gz
-  mkdir -p $FONT_DIR
-  cp -r papirus-icon-theme-master/Papirus* $FONT_DIR \
-    && print_status ok \
-    || print_status ko
-)
+
+print_msg "📥 Copying icons into ${FONT_DIR}"
+copy_files "icons" $FONT_DIR && print_status ok || print_status ko
+
