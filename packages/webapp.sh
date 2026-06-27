b085a1045f55691c338ccfb00dc6e3890d718018
Nicolas COCHIN <github@account.rocks>
2026-06-27 11:20:13 +1000

    Update webapp.sh

--- packages/webapp.sh
+++ packages/webapp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-${BLUEPILL_LOCAL}/bin/create_webapp.sh "WhatsApp" https://web.whatsapp.com/ whatsapp.svg
-${BLUEPILL_LOCAL}/bin/create_webapp.sh "ChatGPT" https://chatgpt.com/ chatgpt.svg
-${BLUEPILL_LOCAL}/bin/create_webapp.sh "Github" https://github.com/ github.svg
+${BLUEPILL_LOCAL}/bin/create_webapp.sh "WhatsApp" https://web.whatsapp.com/ whatsapp
+${BLUEPILL_LOCAL}/bin/create_webapp.sh "ChatGPT" https://chatgpt.com/ chatgpt
+${BLUEPILL_LOCAL}/bin/create_webapp.sh "Github" https://github.com/ github-light
