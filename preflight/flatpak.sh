5fc813f1fc869ca3c426f3daaa15d3b770a015ee
Nicolas COCHIN <github@account.rocks>
2026-06-28 21:00:18 +1000

    Update flatpak.sh

--- preflight/flatpak.sh
+++ preflight/flatpak.sh
@@ -11,7 +11,8 @@ if check_flathub; then
   print_status ok
 else
   print_status ko
+  print_msg "📥 Installing flathub repository"
+  flatpak remote-modify --no-filter --enable flathub && print_status ok || print_status ko
   print_msg "🔧 Enabling flathub"
-  flatpak remote-modify --no-filter --enable flathub
   check_flathub && print_status ok || (print_status ko && exit 1)
 fi
