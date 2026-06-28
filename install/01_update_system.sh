30a869ae0d7e6c19e3445aad9f6d10a0bc076960
Nicolas COCHIN <github@account.rocks>
2026-06-28 19:36:57 +1000

    Delete install directory

--- install/01_update_system.sh
+++
@@ -1,9 +0,0 @@
-echo "Checking if a system update is available"
-
-if rpm-ostree upgrade --check 2> /dev/null | grep -q "AvailableUpdate"
-then     
-  echo "Updating the system"
-  rpm-ostree upgrade --reboot
-else     
-  echo "System is up 2 date"
-fi
