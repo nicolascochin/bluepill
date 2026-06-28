c0a969a61b8dd4175e254950db8305fc642c20ef
Nicolas COCHIN <github@account.rocks>
2026-06-28 21:37:22 +1000

    Update wallpaper.sh

--- config/wallpaper.sh
+++ config/wallpaper.sh
@@ -3,7 +3,7 @@
 WALLPAPER=stormtrooper_keyboard.jpg
 CURRENT_WALLPAPER=${HOME}/Images/current_wallpaper
 
-ln -s ${BLUEPILL_LOCAL}/files/images/${WALLPAPER} ${HOME}/Images/current_wallpaper
+ln -sf ${BLUEPILL_LOCAL}/files/images/${WALLPAPER} ${HOME}/Images/current_wallpaper
 
 print_msg "🛠️ Setting wallpaper light" && gsettings set org.gnome.desktop.background picture-uri "file://$CURRENT_WALLPAPER" && print_status ok || print_status ko
 print_msg "🛠️ Setting wallpaper dark"  && gsettings set org.gnome.desktop.background picture-uri-dark "file://$CURRENT_WALLPAPER" && print_status ok || print_status ko
