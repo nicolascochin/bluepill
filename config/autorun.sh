d58a8f9ebf00d577717967179f95e2fe5bf14c69
Nicolas COCHIN <github@account.rocks>
2026-06-29 11:30:17 +1000

    Create autorun.sh

---
+++ config/autorun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AUTORUN_DIR="${HOME}/.config/autostart"
+FLATPAK_DIR="/var/lib/flatpak/app"
+AUTO_APPS=(
+  "${FLATPAK_DIR}/com.protonvpn.www"
+)
+
+mkdir -p "$AUTORUN_DIR"
+
+for src in "${AUTO_APPS[@]}"; do
+  target="$(basename "$src")"
+  link="${AUTORUN_DIR}/${target}"
+
+  if [[ ! -e "$link" ]]; then
+    print_msg "🚀 Autostart $target" \
+      && ln -s "$src" "$link" \
+      && print_status ok \
+      || print_status ko
+  fi
+done
