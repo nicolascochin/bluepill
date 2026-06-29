6ecd2cde860d14d43e6949b35cd92402f9538379
Nicolas COCHIN <github@account.rocks>
2026-06-30 09:19:54 +1000

    Create logger.sh

---
+++ helpers/logger.sh
@@ -0,0 +1,13 @@
+run_logged() {
+    local description="$1"
+    shift
+
+    print_msg "$description"
+
+    if "$@" >>"$BLUEPILL_LOG" 2>&1; then
+        print_status ok
+    else
+        print_status ko
+        return 1
+    fi
+}
