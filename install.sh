68e20bc468aab4808ae121722604fc41e66a0665
Nicolas COCHIN <github@account.rocks>
2026-06-30 09:21:01 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -Eeuo pipefail
 
 BLUEPILL_LOCAL="${BLUEPILL_LOCAL:-${HOME}/.local/share/bluepill}"
 INSTALL_DIR=${BLUEPILL_LOCAL}/install
+BLUEPILL_LOG="$(mktemp -t bluepill-install-XXXXXX.log)"
 
 # Helpers
 for f in ${BLUEPILL_LOCAL}/helpers/*.sh; do
