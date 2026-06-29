0c539db9e977d14d89de0ae4464e45054c9d47d0
Nicolas COCHIN <github@account.rocks>
2026-06-29 10:03:52 +1000

    Create wezterm.lua

---
+++ files/config/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font "FiraCode Nerd Font Mono"
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.8
+
+return config
