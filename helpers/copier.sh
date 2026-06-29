2dceedea85e16ef5f50261c887a50ae22a63d519
Nicolas COCHIN <github@account.rocks>
2026-06-29 10:08:07 +1000

    Update copier.sh

--- helpers/copier.sh
+++ helpers/copier.sh
@@ -12,3 +12,27 @@ copy_files() {
     cp $f ${dest_folder}/ && print_status ok || print_status ko
   done
 }
+
+copy_file() {
+  local src="${1:?Usage: copy_file <src> <dest>}"
+  local dest="${2:?Usage: copy_file <src> <dest>}"
+  local dest_dir
+
+  dest_dir="$(dirname "$dest")"
+
+  if [[ ! -d "$dest_dir" ]]; then
+    print_msg "📁 Creating directory $dest_dir"
+    mkdir -p "$dest_dir" && print_status ok || {
+      print_status ko
+      return 1
+    }
+  fi
+
+  print_msg "📋 Copying $src to $dest"
+  if cp -- "$src" "$dest"; then
+    print_status ok
+  else
+    print_status ko
+    return 1
+  fi
+}
