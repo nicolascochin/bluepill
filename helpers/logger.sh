e7ae0fcf2b5f62e333489e66a8f728531dc3ea72
Nicolas COCHIN <github@account.rocks>
2026-06-30 16:16:02 +1000

    Update logger.sh

--- helpers/logger.sh
+++ helpers/logger.sh
@@ -1,15 +1,21 @@
 #!/bin/bash 
 
 run_logged() {
-    local description="$1"
-    shift
+  local description="$1"
+  shift
 
-    print_msg "$description"
+  print_msg "$description"
 
-    if "$@" >>"$BLUEPILL_LOG" 2>&1; then
-        print_status ok
-    else
-        print_status ko
-        return 1
-    fi
+  if [[ -n "${BLUEPILL_LOG:-}" ]]; then
+    "$@" >>"$BLUEPILL_LOG" 2>&1
+  else
+    "$@" >/dev/null 2>&1
+  fi
+
+  if [[ $? -eq 0 ]]; then
+    print_status ok
+  else
+    print_status ko
+    return 1
+  fi
 }
