b26d0ea39896687e280f9a2fe8e6d0ca59c433a9
Nicolas COCHIN <github@account.rocks>
2026-06-30 15:53:58 +1000

    Update applications.sh

--- packages/applications.sh
+++ packages/applications.sh
@@ -1,57 +1,53 @@
 #!/usr/bin/env bash
 
 flatpak_install() {
-    local app="$1"
+  local app="$1"
 
-    flatpak install flathub \
-        -y \
-        --noninteractive \
-        "$app"
+  flatpak install flathub \
+    -y \
+    --noninteractive \
+    "$app"
 }
 
 flatpak_installed() {
-    local app="$1"
+  local app="$1"
 
-    flatpak info "$app" >/dev/null 2>&1
+  flatpak info "$app" >/dev/null 2>&1
 }
 
 reinstall_from_flathub() {
-    local app="$1"
+  local app="$1"
 
-    flatpak uninstall -y --noninteractive "$app" &&
-    flatpak_install "$app"
+  flatpak uninstall -y --noninteractive "$app" \
+    && flatpak_install "$app"
 }
 
 declare -A APPS=(
-    ["com.protonvpn.www"]="Proton VPN"
-    ["me.proton.Pass"]="Proton Pass"
-    ["me.proton.Mail"]="Proton Mail"
-    ["com.spotify.Client"]="Spotify"
-    ["org.videolan.VLC"]="VLC"
-    ["com.brave.Browser"]="Brave Browser"
-    ["io.podman_desktop.PodmanDesktop"]="Podman Desktop"
-    ["rest.insomnia.Insomnia"]="Insomnia"
-    ["com.slack.Slack"]="Slack"
-    ["com.discordapp.Discord"]="Discord"
-    ["io.github.CyberTimon.RapidRAW"]="Rapid RAW"
-    ["com.visualstudio.code"]="Visual Studio Code"
-    ["org.wezfurlong.wezterm"]="Wezterm"
+  ["com.protonvpn.www"]="Proton VPN"
+  ["me.proton.Pass"]="Proton Pass"
+  ["me.proton.Mail"]="Proton Mail"
+  ["com.spotify.Client"]="Spotify"
+  ["org.videolan.VLC"]="VLC"
+  ["com.brave.Browser"]="Brave Browser"
+  ["io.podman_desktop.PodmanDesktop"]="Podman Desktop"
+  ["rest.insomnia.Insomnia"]="Insomnia"
+  ["com.slack.Slack"]="Slack"
+  ["com.discordapp.Discord"]="Discord"
+  ["io.github.CyberTimon.RapidRAW"]="Rapid RAW"
+  ["com.visualstudio.code"]="Visual Studio Code"
+  ["org.wezfurlong.wezterm"]="Wezterm"
 )
 
 for app in "${!APPS[@]}"; do
-    if ! flatpak_installed "$app"; then
-        run_logged "📥 Installing ${APPS[$app]}" \
-            flatpak_install "$app" || true
-    fi
+  if ! flatpak_installed "$app"; then
+    run_logged "📥 Installing ${APPS[$app]}" flatpak_install "$app" || true
+  fi
 done
 
 while read -r app; do
-    [[ -z "$app" ]] && continue
+  [[ -z "$app" ]] && continue
 
-    run_logged "📥 Re-installing $app from Flathub" \
-        reinstall_from_flathub "$app"
-
+  run_logged "📥 Re-installing $app from Flathub" reinstall_from_flathub "$app"
 done < <(
-    flatpak list --app --columns=application,origin |
-    awk '$2=="fedora"{print $1}'
+  flatpak list --app --columns=application,origin | awk '$2=="fedora"{print $1}'
 )
