2e6dd8b935a5485d3d77c9c2da18c61d2b352f0f
Nicolas COCHIN <github@account.rocks>
2026-06-30 16:20:39 +1000

    Update shell.sh

--- config/shell.sh
+++ config/shell.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-print_msg "⚙️  Setting ZSH shell"
-chsh --shell /bin/zsh $(whoami) && print_status ok || print_status ko
+run_logged "⚙️  Setting ZSH shell" chsh --shell /bin/zsh $(whoami) 
 
-print_msg "⚙️  Setting ZSH config"
-sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply https://github.com/nicolascochin/dotfiles.git \
-  && print_status ok \
-  || print_status ko
+#print_msg "⚙️  Setting ZSH config"
+#sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply https://github.com/nicolascochin/dotfiles.git \
+#  && print_status ok \
+#  || print_status ko
