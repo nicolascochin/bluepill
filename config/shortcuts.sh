ccd2c6f50de0c7b6b9e798dbeb1a6645c9bfc06f
Nicolas COCHIN <github@account.rocks>
2026-06-30 16:23:39 +1000

    Update shortcuts.sh

--- config/shortcuts.sh
+++ config/shortcuts.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCHEMA="org.gnome.settings-daemon.plugins.media-keys"
 BASE="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
 
-# id|name|command|binding
 SHORTCUTS=(
   "terminal|Terminal|gnome-terminal|<Primary><Alt>t"
   "vscode|VS Code|code|<Primary><Alt>c"
@@ -11,40 +10,32 @@ SHORTCUTS=(
   "distrobox-debian|Distrobox Debian|flatpak run org.wezfurlong.wezterm start -- ${HOME}/.local/bin/distroboxctl run --image debian|<Primary><Shift>F2"
   "distrobox-arch|Distrobox Arch|flatpak run org.wezfurlong.wezterm start -- ${HOME}/.local/bin/distroboxctl run --image archlinux|<Primary><Shift>F3"
   "distrobox-ubuntu|Distrobox Ubuntu|flatpak run org.wezfurlong.wezterm start -- ${HOME}/.local/bin/distroboxctl run --image ubuntu|<Primary><Shift>F4"
-#  "my-script|My Script|/home/$USER/bin/my-script.sh|<Super>F12"
 )
+
 paths=()
 
+configure_shortcut() {
+  local schema="$1"
+  local name="$2"
+  local command="$3"
+  local binding="$4"
+
+  gsettings set "$schema" name "$name" &&
+    gsettings set "$schema" command "$command" &&
+    gsettings set "$schema" binding "$binding"
+}
+
 for shortcut in "${SHORTCUTS[@]}"; do
-    IFS='|' read -r id name command binding <<< "$shortcut"
+  IFS='|' read -r id name command binding <<<"$shortcut"
 
-    path="$BASE/$id/"
-    quoted_path="'$path'"
-    schema="$SCHEMA.custom-keybinding:$path"
+  path="$BASE/$id/"
+  schema="$SCHEMA.custom-keybinding:$path"
+  quoted_path="'$path'"
 
-    # Ajouter le chemin uniquement s'il n'existe pas déjà
-    found=false
+  [[ " ${paths[*]} " =~ ${quoted_path} ]] || paths+=("$quoted_path")
 
-    for p in "${paths[@]}"; do
-        if [[ "$p" == "'$path'" ]]; then
-            found=true
-            break
-        fi
-    done
-
-    if ! $found; then
-        paths+=("$quoted_path")
-        print_msg "🛠️ Adding shortcut: $name"
-    else
-        print_msg "🛠️ Updating shortcut: $name"
-    fi
-
-    command gsettings set "$schema" name "$name" \
-      && command gsettings set "$schema" command "$command" \
-      && command gsettings set "$schema" binding "$binding" \
-      && print_status ok \
-      || print_status ko
+  run_logged "🛠️ Configuring shortcut: $name" configure_shortcut "$schema" "$name" "$command" "$binding" 
 done
 
 list="[$(IFS=,; echo "${paths[*]}")]"
-command gsettings set "$SCHEMA" custom-keybindings "$list"
+run_logged "🛠️ Registering custom shortcuts" gsettings set "$SCHEMA" custom-keybindings "$list" 
