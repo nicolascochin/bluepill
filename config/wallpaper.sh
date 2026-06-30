f31e63b345eae89f1eacd003ded1b88bf8c60b77
Nicolas COCHIN <github@account.rocks>
2026-06-30 16:24:33 +1000

    Update wallpaper.sh

--- config/wallpaper.sh
+++ config/wallpaper.sh
@@ -5,5 +5,5 @@ CURRENT_WALLPAPER=${HOME}/Images/current_wallpaper
 
 ln -sf ${BLUEPILL_LOCAL}/files/images/${WALLPAPER} ${HOME}/Images/current_wallpaper
 
-print_msg "🛠️  Setting wallpaper light" && gsettings set org.gnome.desktop.background picture-uri "file://$CURRENT_WALLPAPER" && print_status ok || print_status ko
-print_msg "🛠️  Setting wallpaper dark"  && gsettings set org.gnome.desktop.background picture-uri-dark "file://$CURRENT_WALLPAPER" && print_status ok || print_status ko
+run_logged "🛠️  Setting wallpaper light" gsettings set org.gnome.desktop.background picture-uri "file://$CURRENT_WALLPAPER" 
+run_logged "🛠️  Setting wallpaper dark"  gsettings set org.gnome.desktop.background picture-uri-dark "file://$CURRENT_WALLPAPER"
