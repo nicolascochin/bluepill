4c9967a2d8671ae7931944c7cb51df175ba5f101
Nicolas COCHIN <github@account.rocks>
2026-06-30 15:57:16 +1000

    Update copier.sh

--- helpers/copier.sh
+++ helpers/copier.sh
@@ -8,8 +8,7 @@ copy_files() {
 
   for f in "${BLUEPILL_LOCAL}/${src_folder}"/*; do
     [[ -f "$f" ]] || continue
-    print_msg "📋 Copying $f into ${dest_folder}"
-    cp $f ${dest_folder}/ && print_status ok || print_status ko
+    run_logged "📋 Copying $f into ${dest_folder}" cp $f ${dest_folder}/ 
   done
 }
 
@@ -22,17 +21,9 @@ copy_file() {
 
   if [[ ! -d "$dest_dir" ]]; then
     print_msg "📁 Creating directory $dest_dir"
-    mkdir -p "$dest_dir" && print_status ok || {
-      print_status ko
-      return 1
-    }
+    run_logged "📁 Creating directory $dest_dir" mkdir -p "$dest_dir"  || return 1
   fi
 
   print_msg "📋 Copying $src to $dest"
-  if cp -- "$src" "$dest"; then
-    print_status ok
-  else
-    print_status ko
-    return 1
-  fi
+  run_logged "📋 Copying $src to $dest" cp -- "$src" "$dest" || return 1
 }
