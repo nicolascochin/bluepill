6d230da2f215616eccaf5b0ed0befd24f9cec532
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-05 22:52:58 +0200

    Update fedora-packages.sh

--- packages/fedora-packages.sh
+++ packages/fedora-packages.sh
@@ -7,4 +7,8 @@ PACKAGES=(
     chezmoi
 )
 
+if has_nvidia_gpu; then
+  PACKAGES+=(akmod-nvidia xorg-x11-drv-nvidia-cuda)
+fi
+
 run_logged "📥 Installing silverblue packages" rpm-ostree install -y --apply-live --idempotent --quiet "${PACKAGES[@]}" 
