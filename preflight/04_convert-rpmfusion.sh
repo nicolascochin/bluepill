0ec63a5a39eb7a5e5b08d4e49eebbdc4c47d495d
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-06 01:23:51 +1000

    Create 04_convert-rpmfusion.sh for RPM Fusion conversion
    Add a script to convert RPM Fusion LocalPackages into LayeredPackages.

---
+++ preflight/04_convert-rpmfusion.sh
@@ -0,0 +1,28 @@
+needs_conversion() {
+    rpm-ostree status --json | jq -e '
+        .deployments[0]["requested-local-packages"]
+        | map(select(
+            startswith("rpmfusion-free-release") or
+            startswith("rpmfusion-nonfree-release")
+          ))
+        | length > 0
+    ' >/dev/null
+}
+
+
+if ! needs_conversion; then
+    print_msg "RPM Fusion converted"
+    print_status ok
+    exit 0
+fi
+
+
+run_logged \
+    "Converting RPM Fusion LocalPackages into LayeredPackages" \
+    sudo rpm-ostree update \
+        --uninstall rpmfusion-free-release \
+        --uninstall rpmfusion-nonfree-release \
+        --install rpmfusion-free-release \
+        --install rpmfusion-nonfree-release
+
+press_enter_to_reboot
