ef7b5f07398308045a54c4065e9fd28af7b8fe35
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-06 01:23:36 +1000

    Create 03_add_rpm-fusion.sh

---
+++ preflight/03_add_rpm-fusion.sh
@@ -0,0 +1,29 @@
+FREE="rpmfusion-free-release"
+NONFREE="rpmfusion-nonfree-release"
+
+is_installed() {
+    rpm-ostree status --json | jq -e --arg pkg "$1" '
+        .deployments[0]
+        | (
+            .packages // []
+            + .requested-packages // []
+            + .requested-local-packages // []
+          )
+        | index($pkg)
+    ' >/dev/null
+}
+
+if is_installed "$FREE" && is_installed "$NONFREE"; then
+    print_msg "RPM Fusion installed"
+    print_status ok
+    exit 0
+fi
+
+
+run_logged \
+    "Installing RPM Fusion repositories" \
+    sudo rpm-ostree install \
+        "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+        "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+press_enter_to_reboot
