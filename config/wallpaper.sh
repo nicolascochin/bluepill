bbe8be1084d71f834f62cf29eaf807d2c3364e3e
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-07 15:39:17 +0200

    Update wallpaper.sh

--- config/wallpaper.sh
+++ config/wallpaper.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-WALLPAPER=stormtrooper_keyboard.jpg
-CURRENT_WALLPAPER=${HOME}/Images/current_wallpaper
-
-ln -sf ${BLUEPILL_LOCAL}/files/images/${WALLPAPER} ${HOME}/Images/current_wallpaper
-
-run_logged "🛠️  Setting wallpaper light" gsettings set org.gnome.desktop.background picture-uri "file://$CURRENT_WALLPAPER" 
-run_logged "🛠️  Setting wallpaper dark"  gsettings set org.gnome.desktop.background picture-uri-dark "file://$CURRENT_WALLPAPER"
+run_logged "🔗  Link wallpapers" ln -fs ~/.local/share/bluepill/files/wallpapers/ ~/Images/Wallpapers
+run_logged "🛠️  Setting wallpaper" ${BLUEPILL_LOCAL}/files/bin/change_wallpaper
