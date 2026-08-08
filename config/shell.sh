650b2e2f912b218a60fe290ab248bd443b157c9a
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-08 16:52:12 +0200

    Update shell.sh

--- config/shell.sh
+++ config/shell.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-run_logged "⚙️  Setting ZSH shell" pkexec chsh --shell /bin/zsh $(whoami) 
+TARGET_SHELL="/bin/zsh"
+CURRENT_SHELL="$(getent passwd "$(whoami)" | cut -d: -f7)"
+
+if [[ "$CURRENT_SHELL" == "$TARGET_SHELL" ]]; then
+  print_status "ok" "ZSH déjà configuré comme shell par défaut"
+else
+  run_logged "⚙️  Setting ZSH shell" pkexec chsh --shell "$TARGET_SHELL" "$(whoami)"
+fi
 
 #print_msg "⚙️  Setting ZSH config"
 #sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply https://github.com/nicolascochin/dotfiles.git \
