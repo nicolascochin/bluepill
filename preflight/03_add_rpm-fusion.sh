78774f215252a8b9ac95521b1be9b5fcccc466d7
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-08 09:31:15 +0200

    Add emoji to RPM Fusion installation messages

--- preflight/03_add_rpm-fusion.sh
+++ preflight/03_add_rpm-fusion.sh
@@ -14,11 +14,11 @@ is_installed() {
 }
 
 if is_installed "$FREE" && is_installed "$NONFREE"; then
-    print_msg "RPM Fusion installed"
+    print_msg "📦  RPM Fusion installed"
     print_status ok
 else
   run_logged \
-    "Installing RPM Fusion repositories" \
+    "📦  Installing RPM Fusion repositories" \
     sudo rpm-ostree install \
       "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
       "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
