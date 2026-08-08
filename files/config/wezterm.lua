03e1588021f49d8f6e01f7f4b3059ec5d1434926
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-08 23:14:45 +0200

    Update wezterm.lua

--- files/config/wezterm.lua
+++ files/config/wezterm.lua
@@ -2,7 +2,11 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font "FiraCode Nerd Font Mono"
-config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.8
+
+config.color_scheme = "Dracula"
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
 
 return config
