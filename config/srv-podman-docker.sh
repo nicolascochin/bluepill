a0855a61a9bb3229456c8f6115c49ec73336a1bf
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-08 20:27:02 +1000

    Modify copy_file command in srv-podman-docker.sh
    Updated copy_file command to include 'true' argument.

--- config/srv-podman-docker.sh
+++ config/srv-podman-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ${HOME}/.config/systemd/user
-copy_file ${BLUEPILL_LOCAL}/files/systemd/system/podman-docker-symlink.service /etc/systemd/system/podman-docker-symlink.service
+copy_file ${BLUEPILL_LOCAL}/files/systemd/system/podman-docker-symlink.service /etc/systemd/system/podman-docker-symlink.service true
 
 run_logged "🔄 Reloading systemd user daemon" sudo systemctl daemon-reload 
 run_logged "📦 Enabling Podman-Docker symlink" sudo systemctl --now podman-docker-symlink.service
