c269b7965af2fa21c213948cf378fbf9397deb9b
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-08 09:34:50 +0200

    Update remove_firefox.sh

--- preflight/remove_firefox.sh
+++ preflight/remove_firefox.sh
@@ -9,7 +9,7 @@ if ! is_firefox_installed; then
   print_status ok
 else
   run_logged \
-    "🗑️  Removing native Firefox (keeping Flatpak version)" \
+    "🗑️  Removing native Firefox" \
     sudo rpm-ostree override remove firefox firefox-langpacks
 
   press_enter_to_reboot
