4889622e0a2adddf9e3c74a804f9694ddae5c1fb
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-08 14:43:44 +0200

    Delete config/srv-podman-docker.sh

--- config/srv-podman-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-copy_file ${BLUEPILL_LOCAL}/files/systemd/system/podman-docker-symlink.service /etc/systemd/system/podman-docker-symlink.service true
-
-run_logged "🔄 Reloading systemd user daemon" sudo systemctl daemon-reload 
-run_logged "📦 Enabling Podman-Docker symlink" sudo systemctl enable --now podman-docker-symlink.service
