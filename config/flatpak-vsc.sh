2cb29d9692879d9aaf993f15b63c74e91c68876b
Nicolas COCHIN <11385737+nicolascochin@users.noreply.github.com>
2026-08-12 22:14:37 +1000

    Update flatpak-vsc.sh

--- config/flatpak-vsc.sh
+++ config/flatpak-vsc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+_pds_conf="/etc/tmpfiles.d/podman-docker.conf"
+_pds_sock="/run/user/${UID}/podman/podman.sock"
+
 run_logged "⚙️ Enable podman socket" systemctl --user enable --now podman.socket
 run_logged "🛠️ Configure VSC" flatpak override \
   --user com.visualstudio.code \
@@ -7,4 +10,7 @@ run_logged "🛠️ Configure VSC" flatpak override \
   --filesystem=/tmp \
   --env=DOCKER_HOST=unix:///run/user/1000/podman/podman.sock
 
-  run_logged "🔗 Create a symlink docker" sudo ln -sf /run/user/1000/podman/podman.sock /var/run/docker.sock
+  # run_logged "🔗 Create a symlink docker" sudo ln -sf /run/user/1000/podman/podman.sock /var/run/docker.sock
+
+run_logged "🔗 Create file ${_pds_conf}" printf 'L+ /run/docker.sock - - - - %s\n' "${_pds_sock}" | sudo tee "${_pds_conf}" >/dev/null
+run_logged "🔗 Apply config file" sudo systemd-tmpfiles --create "${_pds_conf}"
