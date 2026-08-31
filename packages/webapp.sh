7d20f12ad6a75c830335703cdf6e0c0f9c54e447
Nicolas COCHIN <github@onid.mx>
2026-08-31 21:40:42 +0200

    Add IO Center webapp creation command

--- packages/webapp.sh
+++ packages/webapp.sh
@@ -4,3 +4,4 @@ ${BLUEPILL_LOCAL}/files/bin/create_webapp "WhatsApp" https://web.whatsapp.com/ w
 ${BLUEPILL_LOCAL}/files/bin/create_webapp "ChatGPT" https://chatgpt.com/ chatgpt
 ${BLUEPILL_LOCAL}/files/bin/create_webapp "Claude" "https://claude.ai" claude-ai
 ${BLUEPILL_LOCAL}/files/bin/create_webapp "Proton Mail" "https://mail.proton.me" proton-mail
+${BLUEPILL_LOCAL}/files/bin/create_webapp "IO Center" "https://iocenter.bequiet.com" keybr
